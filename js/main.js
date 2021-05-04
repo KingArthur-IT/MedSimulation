5cedd83d5fcaf7bef113283f9d567931541930d7
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-05-04 15:13:18 +0300

    pen control by mouse

--- js/main.js
+++ js/main.js
@@ -23,56 +23,88 @@ window.onload = function () {
     });
 
     //objects
-    var penObj = new THREE.Object3D();
+    let penObj = new THREE.Object3D();
+    let baseObj = new THREE.Object3D();
     let mtlLoader = new THREE.MTLLoader();
     mtlLoader.setPath("../assets/models/");
+    //load pen
     mtlLoader.load('bovie.mtl', function(materials) {
-        //materials.preload();
+        materials.preload();
         let objLoader = new THREE.OBJLoader();
         objLoader.setMaterials(materials);
         objLoader.setPath("../assets/models/");
         objLoader.load('bovie.obj', function (object) {
-            object.scale.set(30, 30, 30);
+            object.scale.set(40, 40, 40);
+            object.position.set(0, 0, 0);
+            object.rotation.set(Math.PI / 2.0, 10, 0);
             penObj.add(object);
-            //mesh = object;
             scene.add(penObj);
         });
     });
-/*
-    const loader2 = new THREE.OBJLoader();
-    // load a resource
-    loader2.load(
-        // resource URL
-        './assets/models/bovie.obj',
-        // called when resource is loaded
-        function (object) {
-            scene.add( object );
-        },
-        // called when loading is in progresses
-        function ( xhr ) {
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        },
-        // called when loading has errors
-        function ( error ) {
-            console.log( 'An error happened' );
-        }
-    );
-*/
-
+    let penInitialParams = {
+        angleX: - Math.PI / 6.0,
+        angleY: 0.0,
+        angleZ: 0.0
+    }
+    function startPenObject() {
+        penObj.rotation.x = penInitialParams.angleX;
+        penObj.rotation.y = penInitialParams.angleY;
+        penObj.rotation.z = penInitialParams.angleZ;
+    }; 
+    startPenObject();
+    //load base
+    mtlLoader.load('base.mtl', function(materials) {
+        materials.preload();
+        let objLoader = new THREE.OBJLoader();
+        objLoader.setMaterials(materials);
+        objLoader.setPath("../assets/models/");
+        objLoader.load('base.obj', function (object) {
+            object.scale.set(70, 70, 70);
+            object.position.set(0, 0, 0);
+            object.rotation.set(Math.PI / 2.0, 0, 0);
+            baseObj.add(object);
+            scene.add(baseObj);
+        });
+    });
+    
+    //main render loop
     function loop() {
-        //console.log(penObj);
-        penObj.rotation.z += Math.PI / 100;
-        penObj.rotation.x += Math.PI / 100;
-        //objMesh.rotation.z +=  Math.PI / 100;
         renderer.render(scene, camera);
         requestAnimationFrame(loop)
-    }
-
+    };
     loop();
         
-    
+    //mouse
+    let mouseObj = {
+        isDown: false
+    }
     canvas.addEventListener("mousemove", mouse_move_handler);
+    canvas.addEventListener("mousedown", mouse_down_handler);
+    canvas.addEventListener("mouseup", mouse_up_handler);
+    
     function mouse_move_handler(e) {
+        if (!mouseObj.isDown) return;
+        let centerX = width / 2.0,
+            centerY = height / 2.0,
+            R = 200,
+            maxAngle = Math.PI / 4.0;
         
+        let yAngle = maxAngle * mod( (centerX - e.x) / R );
+        let xAngle = maxAngle * mod( (e.y - centerY) / R );
+        penObj.rotation.y = -yAngle;
+        penObj.rotation.x = xAngle;
+    }
+
+    function mouse_down_handler() {
+        mouseObj.isDown = true;
+    }
+    
+    function mouse_up_handler() {
+        mouseObj.isDown = false;
+        startPenObject();
+    }
+
+    function mod(x) { //x set -1 or 1 if it is out of interval [-1; 1] 
+        return Math.abs(x) > 1 ? 1.0 * x / Math.abs(x) : x;
     }
 }
