f569673c72ff556f9b1eea61174d600c869f6faf
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-05-04 09:40:19 +0300

    base rotate pen obj

--- js/main.js
+++ js/main.js
@@ -9,43 +9,35 @@ window.onload = function () {
     //renderer
     let renderer = new THREE.WebGLRenderer({ canvas: canvas });
     renderer.setClearColor(0x000000);
-
     //scene and camera
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000); //width / height
     camera.position.set(0, 0, 1000);
-
     //light
     let light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
-
-    //geometries
-    const bgPlane = new THREE.PlaneGeometry(width, height, 10.0);
-    
-    //loader
-    const loader = new THREE.TextureLoader();
-    const material = new THREE.MeshBasicMaterial({
-        map: loader.load('./assets/img/interaction_bg.jpg')
+    //Load background texture
+    let loader = new THREE.TextureLoader();
+    loader.load('./assets/img/interaction_bg.jpg' , function(texture){
+        scene.background = texture;  
     });
 
-    //mesh
-    let mesh = new THREE.Mesh(bgPlane, material);
-    scene.add(mesh);
-
-    var mtlLoader = new THREE.MTLLoader();
+    //objects
+    var penObj = new THREE.Object3D();
+    let mtlLoader = new THREE.MTLLoader();
     mtlLoader.setPath("../assets/models/");
     mtlLoader.load('bovie.mtl', function(materials) {
-            //materials.preload();
-            var objLoader = new THREE.OBJLoader();
-            objLoader.setMaterials(materials);
-            objLoader.setPath("../assets/models/");
-            objLoader.load('bovie.obj', function(object) {
-                object.scale.set(30, 30, 30);
-                mesh = object;
-                scene.add(mesh);
-            });
+        //materials.preload();
+        let objLoader = new THREE.OBJLoader();
+        objLoader.setMaterials(materials);
+        objLoader.setPath("../assets/models/");
+        objLoader.load('bovie.obj', function (object) {
+            object.scale.set(30, 30, 30);
+            penObj.add(object);
+            //mesh = object;
+            scene.add(penObj);
         });
-
+    });
 /*
     const loader2 = new THREE.OBJLoader();
     // load a resource
@@ -68,7 +60,10 @@ window.onload = function () {
 */
 
     function loop() {
-        //mesh.rotation.y += Math.PI / 100;
+        //console.log(penObj);
+        penObj.rotation.z += Math.PI / 100;
+        penObj.rotation.x += Math.PI / 100;
+        //objMesh.rotation.z +=  Math.PI / 100;
         renderer.render(scene, camera);
         requestAnimationFrame(loop)
     }
