4efe664b6af4a1ff90221a870c803bfc49650b37
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-05-16 08:12:21 +0300

    remove console

--- js/main.js
+++ js/main.js
@@ -471,7 +471,6 @@ window.onload = function () {
                 Math.abs(simulation.penCoords.y - simulation.trainingCheckPoints.secondChange.y) < simulation.trainingCheckPoints.accuracy)) {
             if (simulation.changeIndex) {
                 simulation.changeIndex = false;
-                console.log(simulation.changeIndex, simulation.dataIndex)
                 simulation.dataIndex = simulation.dataIndex == 0 ? 1 : 0;
             }
             }
